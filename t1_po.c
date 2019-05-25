cd4e8f784f6306c79a1c75d132053d3ec389fb1b
Ricardo Freitas <ricardo-cadastros@hotmail.com>
2019-05-25 16:23:45 -0300

    Adicao do codigo-fonte

---
+++ t1_po.c
@@ -0,0 +1,16 @@
+/*
+ * 1º Trabalho de Pesquisa Operacional.
+ * Integrantes do grupo:
+ * Nome: Alexandre Saura.
+ * Nome: Fábio Thomaz.
+ * Nome: Ricardo Oliveira.
+ * Profº.: José Fontebasso Neto.
+ * Discplina: Pesquisa Operacional - I.
+ * Universidade  Católica de Santos - Maio de 2019.
+*/
+#include <stdio.h>
+
+int main()
+{
+	return 0;
+}
